daee26e0b066f99fdcc6ac46b2bdccc7b4b5a7d3
Nikita <flwork.poshta@gmail.com>
2020-06-16 16:44:35 +0300

    fourth hm with object

--- js/script.js
+++ js/script.js
@@ -1,71 +1,96 @@
-"use strict";
+ "use strict";
 
- let numberofFilms; 
-
- function start() {
-    numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
-
-    while (numberofFilms == '' || numberofFilms == null || isNaN(numberofFilms)){
-        numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
-    }
- }
- start(); 
+  
 const personalMovieDB = {
-    count: numberofFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?','');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?','');
+        }
+     },
+     rememberMyfilmes: function(){
+        for (let i = 0; i<2; i++){
+            const a = prompt('Один из последних просмотренных фильмов?',''),
+             b = prompt ('На сколько оцените его?','');
+         
+             if (a != null && b != null && a !='' && b != '' && a.length < 50){
+                 personalMovieDB.movies[a] = b;
+                 console.log('Done!');
+             } else{
+                 console.log('Error!');
+                 i--;
+             }
+         }
+    },
+    detectPersonalLevel:function(){
+        if (personalMovieDB.count < 10){
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (10 < personalMovieDB.count < 30 ) {
+            console.log('Вы классический зритель');
+        }else if (personalMovieDB.count > 30){
+            console.log('Вы киноман');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+    showMyDB: function(hidden){
+        if (!hidden){
+            console.log(personalMovieDB);
+        }
+    },
+    toogleVisibleDB: function(){
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat =false;
+        } else {
+            personalMovieDB.privat =true;
+        }
+    },
+    writeYourGenres: function(){
+        for (let i=1;i<=3;i++){
+            let genre = prompt(`Ваш любимый жанр под номером${i} `,'');
+            
+          if (genre === null || genre ==- ''){
+                 console.log('Error data');
+                 i--;
+          } else{
+            personalMovieDB.genres[i-1] = genre;
+          }
+        }
+        personalMovieDB.genres.sort();
+        personalMovieDB.genres.forEach((item, i)=>{
+            console.log(`Улюблений жанр ${i+1} - це ${item}`);
+        });
+    }
 };
 
-
-function rememberMyfilmes(){
-    for (let i = 0; i<2; i++){
-        const a = prompt('Один из последних просмотренных фильмов?',''),
-         b = prompt ('На сколько оцените его?','');
-     
-         if (a != null && b != null && a !='' && b != '' && a.length < 50){
-             personalMovieDB.movies[a] = b;
-             console.log('Done!');
-         } else{
-             console.log('Error!');
-             i--;
-         }
-     }
-}
- rememberMyfilmes(); 
+ 
      
 
 
 
-function detectPersonalLevel(){
-    if (personalMovieDB.count < 10){
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (10 < personalMovieDB.count < 30 ) {
-        console.log('Вы классический зритель');
-    }else if (personalMovieDB.count > 30){
-        console.log('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
-detectPersonalLevel(); 
 
 
-function showMyDB(){
-    personalMovieDB.privat = prompt('Показать результат?');
-    if (personalMovieDB.privat !== null) {
-        console.log(personalMovieDB);
-    } else {
-        console.log('Ok,no problem');
-    }
-}
-showMyDB();
 
-function writeYourGenres(){
-    for (let i=1;i<=3;i++){
-      personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером${i} `,'');
-    }
-}
+ 
 
-writeYourGenres();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
